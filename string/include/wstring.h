a79698f1627aa31df27bc48b54ba3a8b65066ad5
Artur <vags2154@gmail.com>
2024-02-08 22:46:20 +0100

    fix bag

--- string/include/wstring.h
+++ string/include/wstring.h
@@ -1,9 +1,9 @@
-#ifndef A_STRING_H
-#define A_STRING_H
+#ifndef WSTRING_H
+#define WSTRING_H
 
 #include <iostream>
 #include <algorithm>
-#include <initializer_list> // C++11
+#include <initializer_list> 
 #include <cstring>
 
 
@@ -13,11 +13,11 @@ private:
     size_t size = 0;
     size_t capacity = 0;
 private:
-    String(size_t n): arr(new char[n + 1]), size(n), capacity(n + 1) { // Delegating constructor (C++11)
+    String(size_t n): arr(new char[n + 1]), size(n), capacity(n + 1) {
         arr[size] = '\0';
     }
 public:
-    String() = default; // C++11
+    String() = default;
     
     String(size_t n, size_t c): String(n) {
         memset(arr, c, n);
@@ -46,4 +46,4 @@ public:
     }
 };
 
-#endif // A_STRING_H
+#endif // WSTRING_H
