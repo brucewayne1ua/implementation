87d556ecd4dc715c28ec0d16ecff343921495cd4
Artur <vags2154@gmail.com>
2024-02-12 22:10:12 +0100

    Modified main.cpp

--- string/src/main.cpp
+++ string/src/main.cpp
@@ -1,8 +1,22 @@
+/**
+ * Implementation of std::string.
+ * The main.cpp file will only write tests for my std::string implementation.
+ * Also, anyone can write their own tests in my code and request a "Pull Request"
+ */
 
 
 #include "../include/wstring.h"
 
-int main() {
+
+/* Copy constructor */
+void first_test()
+{
     String s = { 'a', 'b', 'c' };
     String s2 = s;
+}
+
+
+int main(int argc, char* argv[]) {
+    first_test();
+
 }
