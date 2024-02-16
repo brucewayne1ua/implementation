b70bbfafddee987dd272f43f9e2ece388d4e1361
Artur <vags2154@gmail.com>
2024-02-16 23:02:11 +0100

    add operator+ and rsl::String::length()

---
+++ string/include/string.hpp
@@ -0,0 +1,97 @@
+#ifndef STRING_HPP
+#define STRING_HPP
+
+
+#include <iostream>
+#include <algorithm>
+#include <initializer_list> 
+#include <cstring>
+
+namespace rsl
+{
+
+    class String
+    {
+    private:
+
+        char* arr = nullptr;
+        size_t size = 0;
+        size_t capacity = 0;
+
+    private:
+
+        String(size_t n): arr(new char[n + 1]), size(n), capacity(n + 1)
+        {
+            arr[size] = '\0';
+        }
+
+    public:
+        /* Constructors */
+        String() = default;
+    
+
+        String(size_t n, size_t c): String(n)
+        {
+            std::memset(arr, c, n);
+        }
+
+
+        String(std::initializer_list<char> list): String(list.size())
+        {
+            std::copy(list.begin(), list.end(), arr);
+        }
+
+
+        String(const String& other): String(other.size)
+        {
+            std::memcpy(arr, other.arr, size + 1);
+        }
+
+
+        ~String()
+        {
+            delete[] arr;
+        }
+
+
+    public:
+        /* Operators */
+        String& operator=(const String& other)
+        {
+            if (this == &other) return *this; 
+            delete[] arr;
+            size = other.size;
+            capacity = other.capacity;
+            arr = new char[other.capacity];
+            std::memcpy(arr, other.arr, size + 1);
+            return *this;
+        }
+
+
+        String operator+(const String& other) const 
+        {
+            String result;
+            result.size = size + other.size;
+            result.capacity = result.size; 
+            result.arr = new char[result.capacity + 1];
+            std::memcpy(result.arr, arr, size);
+            std::memcpy(result.arr + size, other.arr, other.size + 1);
+            return result;
+        }
+
+
+    public:
+        /* Functions */
+        int length()
+        {
+            return size;
+        }
+
+
+    };
+
+
+} // namespace rsl
+
+
+#endif // STRING_HPP
--- string/src/main.cpp
+++ string/src/main.cpp
@@ -5,18 +5,22 @@
  */
 
 
-#include "../include/wstring.h"
+#include "../include/string.hpp"
 
 
 /* Copy constructor */
 void first_test()
 {
-    String s = { 'a', 'b', 'c' };
-    String s2 = s;
+    rsl::String s = { 'a', 'b', 'c' }; 
+    rsl::String s2 = { 'a', 'b', 'c', 'd', 'e' }; 
+    std::cout << "s2: " << s2.length() << '\n';
+    rsl::String s3 = s2 + s;
+    std::cout << "s3: " << s3.length() << '\n';
 }
 
 
-int main(int argc, char* argv[]) {
+int main(int argc, char** argv)
+{
     first_test();
-
+    return 0;
 }
