393d6a0d441dcbdd7a47c5856a5d16abd563e668
Artur <vags2154@gmail.com>
2024-02-20 22:41:19 +0200

    clean up

--- string/include/string.hpp
+++ string/include/string.hpp
@@ -20,29 +20,29 @@ namespace rsl
 
     private:
 
-        String(size_t n): arr(new char[n + 1]), size(n), capacity(n + 1)
+        String(size_t n) : arr(new char[n + 1]), size(n), capacity(n + 1)
         {
             arr[size] = '\0';
         }
 
     public:
-           
-        String() = default;
-    
 
-        String(size_t n, size_t c): String(n)
+        String() = default;
+
+
+        String(size_t n, size_t c) : String(n)
         {
             std::memset(arr, c, n);
         }
 
 
-        String(std::initializer_list<char> list): String(list.size())
+        String(std::initializer_list<char> list) : String(list.size())
         {
             std::copy(list.begin(), list.end(), arr);
         }
 
 
-        String(const String& other): String(other.size)
+        String(const String& other) : String(other.size)
         {
             std::memcpy(arr, other.arr, size + 1);
         }
@@ -55,10 +55,10 @@ namespace rsl
 
 
     public:
-        
+
         String& operator=(const String& other)
         {
-            if (this == &other) return *this; 
+            if (this == &other) return *this;
             delete[] arr;
             size = other.size;
             capacity = other.capacity;
@@ -68,11 +68,11 @@ namespace rsl
         }
 
 
-        String operator+(const String& other) const 
+        String operator+(const String& other) const
         {
             String result;
             result.size = size + other.size;
-            result.capacity = result.size; 
+            result.capacity = result.size;
             result.arr = new char[result.capacity + 1];
             std::memcpy(result.arr, arr, size);
             std::memcpy(result.arr + size, other.arr, other.size + 1);
@@ -81,7 +81,7 @@ namespace rsl
 
 
     public:
-        
+
         int length()
         {
             return size;
@@ -91,4 +91,4 @@ namespace rsl
     };
 
 
-} 
+};
